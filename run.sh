c388a4f70b55a414b0302c0d3112e5b7483e6e47
Jebaitedneko <Jebaitedneko@gmail.com>
2021-08-15 03:40:03 +0530

    simplify node labelling via relabel function

--- run.sh
+++ run.sh
@@ -9,9 +9,14 @@ mv dtb/*.dtb . && rm -rf dtb
 grep "Qualcomm Technologies, Inc. SM8150 v2 SoC" *.dtb &> match
 mv "$(cat match | cut -f2 -d: | column -t)" dtb && rm match && rm *.dtb
 dtc -I dtb -O dts -o dts dtb &> /dev/null && rm dtb
-sed -i "s/soc {/soc: soc {/g" "dts"
-sed -i "s/lmh-dcvs-00 {/lmh_0: lmh-dcvs-00 {/g" "dts"
-sed -i "s/lmh-dcvs-01 {/lmh_1: lmh-dcvs-01 {/g" "dts"
+function label() {
+	sed -i "s/$1 {/$2: $1 {/g" "dts"
+}
+label "soc" "soc"
+label "gpu_opp_table_v2" "gpu_opp"
+label "qcom,gpu-pwrlevels-1" "gpu_pwrlvl"
+label "lmh-dcvs-00" "lmh_0"
+label "lmh-dcvs-01" "lmh_1"
 cat "mod.dtsi" >> "dts"
 sed -i "s/==> dts <==//g" "dts"
 dtc -I dts -O dtb "dts" -o "dtb" &> /dev/null && rm "dts"
