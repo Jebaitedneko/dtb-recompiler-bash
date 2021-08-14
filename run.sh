3d4e31129788198484a015210f8bdf749d513ed1
Jebaitedneko <Jebaitedneko@gmail.com>
2021-08-15 05:08:01 +0530

    run.sh: shellcheck fixes

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@
 [[ ! -f boot.img ]] && echo -e "Run\n\n\nsu\n\nsh img.sh\n\nexit\n\n\nand then re-run run.sh"  && exit
 chmod +x dtb.py && python dtb.py boot.img &> /dev/null
 mv dtb/*.dtb . && rm -rf dtb
-grep "Qualcomm Technologies, Inc. SM8150 v2 SoC" *.dtb &> match
-mv "$(cat match | cut -f2 -d: | column -t)" dtb && rm match && rm *.dtb
+grep "Qualcomm Technologies, Inc. SM8150 v2 SoC" ./*.dtb &> match
+mv "$(cut -f2 -d: < match | column -t)" "dtb" && rm match && rm ./*.dtb
 dtc -I dtb -O dts -o dts dtb &> /dev/null && rm dtb
 function label() {
 	sed -i "s/$1 {/$2: $1 {/g" "dts"
@@ -22,4 +22,4 @@ sed -i "s/==> dts <==//g" "dts"
 dtc -I dts -O dtb "dts" -o "dtb" &> /dev/null && rm "dts"
 echo "Done."
 [[ -f dtb-mod.zip ]] && rm dtb-mod.zip
-mv dtb ak3 && ( cd ak3 && zip -r9 ../dtb-mod.zip * &> /dev/null ) && rm ak3/dtb
+mv dtb ak3 && ( cd ak3 && zip -r9 ../dtb-mod.zip ./* &> /dev/null ) && rm ak3/dtb
