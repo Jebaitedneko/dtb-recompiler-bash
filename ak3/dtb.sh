b0492259bbf2f604f8025b12d7c79fb16123de8b
Jebaitedneko <Jebaitedneko@gmail.com>
2022-02-07 02:37:12 +0530

    workaround

--- ak3/dtb.sh
+++ ak3/dtb.sh
@@ -25,7 +25,7 @@ common() {
 unpack_dtb() {
     dir_check
     $MAGISKBOOT unpack "$1" &> /dev/null && mv dtb dtb.pack && rm kernel ramdisk.cpio
-    xxd -g0 dtb.pack | cut -f2 -d' ' | cut -f1 -d ' ' | tr -d '\n' | sed -E 's/(d00dfeed)/\n\1/g' > dtb.pack.xxd
+    xxd -g2 dtb.pack | sed 's/ //g' | cut -c10- | cut -c-32 | tr -d '\n' | sed -E 's/(d00dfeed)/\n\1/g' > dtb.pack.xxd
     sed -i '1d' dtb.pack.xxd
     grep d00dfeed dtb.pack.xxd | while read f; do i=$((i+1)); echo "$f" > .tmp; xxd -r -p < .tmp > dtbs/$i.dtb; rm .tmp; done
     rm -rf dtb.pack dtb.pack.xxd
